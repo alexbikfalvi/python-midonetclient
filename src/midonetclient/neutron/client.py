3a365658fd5a06791d7dbe705098f1d1aa2fee9d
Ryu Ishimoto <ryu@midokura.com>
2014-02-06 20:58:28 +0900

    Add support for security groups and network bulk create
    Add interface to do bulk create on network, as well as all the
    operations for security groups.
    
    Signed-off-by: Ryu Ishimoto <ryu@midokura.com>

--- src/midonetclient/neutron/client.py
+++ src/midonetclient/neutron/client.py
@@ -30,7 +30,15 @@ class MediaType(object):
     APP = "application/vnd.org.midonet.Application-v3+json"
     NEUTRON = "application/vnd.org.midonet.neutron.Neutron-v1+json"
     NETWORK = "application/vnd.org.midonet.neutron.Network-v1+json"
-    NETWORKS = "application/vnd.org.midonet.neutron.collection.Network-v1+json"
+    NETWORKS = "application/vnd.org.midonet.neutron.Networks-v1+json"
+    SECURITY_GROUP = \
+        "application/vnd.org.midonet.neutron.SecurityGroup-v1+json"
+    SECURITY_GROUPS = \
+        "application/vnd.org.midonet.neutron.SecurityGroups-v1+json"
+    SECURITY_GROUP_RULE = \
+        "application/vnd.org.midonet.neutron.SecurityGroupRule-v1+json"
+    SECURITY_GROUP_RULES = \
+        "application/vnd.org.midonet.neutron.SecurityGroupRules-v1+json"
 
 
 class UrlProvider(object):
@@ -57,6 +65,20 @@ class UrlProvider(object):
     def networks_url(self):
         return self._get_neutron()["networks"]
 
+    def security_group_url(self, id):
+        return self._get_neutron()["security_group_template"].replace(
+            "{id}", id)
+
+    def security_groups_url(self):
+        return self._get_neutron()["security_groups"]
+
+    def security_group_rule_url(self, id):
+        return self._get_neutron()["security_group_rule_template"].replace(
+            "{id}", id)
+
+    def security_group_rules_url(self):
+        return self._get_neutron()["security_group_rules"]
+
 
 class MidonetClient(UrlProvider):
 
@@ -70,6 +92,11 @@ class MidonetClient(UrlProvider):
         LOG.info("create_network %r", network)
         return self.client.post(self.networks_url(), MediaType.NETWORK,
                                 body=network)
+
+    def create_network_bulk(self, networks):
+        LOG.info("create_network_bulk entered")
+        return self.client.post(self.networks_url(), MediaType.NETWORKS,
+                                body=networks)
 
     def delete_network(self, id):
         LOG.info("delete_network %r", id)
@@ -86,3 +113,55 @@ class MidonetClient(UrlProvider):
     def update_network(self, id, network):
         LOG.info("update_network %r", network)
         self.client.put(self.network_url(id), MediaType.NETWORK, network)
+
+    def create_security_group(self, security_group):
+        LOG.info("create_security_group %r", security_group)
+        return self.client.post(self.security_groups_url(),
+                                MediaType.SECURITY_GROUP,
+                                body=security_group)
+
+    def create_security_group_bulk(self, security_groups):
+        LOG.info("create_security_group_bulk entered")
+        return self.client.post(self.security_groups_url(),
+                                MediaType.SECURITY_GROUPS,
+                                body=security_groups)
+
+    def delete_security_group(self, id):
+        LOG.info("delete_security_group %r", id)
+        self.client.delete(self.security_group_url(id))
+
+    def get_security_group(self, id):
+        LOG.info("get_security_group %r", id)
+        return self.client.get(self.security_group_url(id),
+                               MediaType.SECURITY_GROUP)
+
+    def list_security_groups(self):
+        LOG.info("list_security_groups")
+        return self.client.get(self.security_groups_url(),
+                               MediaType.SECURITY_GROUPS)
+
+    def update_security_group(self, id, security_group):
+        LOG.info("update_security_group %r", security_group)
+        self.client.put(self.security_group_url(id), MediaType.SECURITY_GROUP,
+                        security_group)
+
+    def create_security_group_rule(self, security_group_rule):
+        LOG.info("create_security_group_rule %r", security_group_rule)
+        return self.client.post(self.security_group_rules_url(),
+                                MediaType.SECURITY_GROUP_RULE,
+                                body=security_group_rule)
+
+    def create_security_group_rule_bulk(self, security_group_rules):
+        LOG.info("create_security_group_rule_bulk entered")
+        return self.client.post(self.security_group_rules_url(),
+                                MediaType.SECURITY_GROUP_RULES,
+                                body=security_group_rules)
+
+    def delete_security_group_rule(self, id):
+        LOG.info("delete_security_group_rule %r", id)
+        self.client.delete(self.security_group_rule_url(id))
+
+    def get_security_group_rule(self, id):
+        LOG.info("get_security_group_rule %r", id)
+        return self.client.get(self.security_group_rule_url(id),
+                               MediaType.SECURITY_GROUP_RULE)
