12eb4bc5f5bde2ebad29fb66ac3048797ac02f50
Ryu Ishimoto <ryu@midokura.com>
2014-02-06 23:44:19 +0900

    Add security groups test
    Implement simple security groups test for sanity checks.
    
    Signed-off-by: Ryu Ishimoto <ryu@midokura.com>

--- src/midonetclient/neutron/func_tests.py
+++ src/midonetclient/neutron/func_tests.py
@@ -20,6 +20,7 @@
 import logging
 import sys
 import uuid
+import webob.exc as exc
 
 from midonetclient.neutron.client import MidonetClient
 
@@ -29,22 +30,170 @@ LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.DEBUG)
 
 
+def test_network(client):
+
+    print '-------- Testing Network CRUD-----'
+    test_network_crud(client)
+
+    print '-------- Testing Network Bulk-----'
+    test_network_bulk(client)
+
+
+def test_security_group(client):
+
+    print '-------- Testing Security Group CRUD-----'
+    test_security_group_crud(client)
+
+    print '-------- Testing Security Group Bulk-----'
+    test_security_group_bulk(client)
+
+
 def test_network_crud(client):
-    net_id = str(uuid.uuid4())
-    net = {"id": net_id,
-           "name": net_id + "name",
-           "tenant_id": "foo",
-           "admin_state_up:": True}
-    net = client.create_network(net)
-    assert net_id == net["id"]
+    id = str(uuid.uuid4())
+    input = {"id": id,
+             "name": "name",
+             "tenant_id": "tenant_id",
+             "admin_state_up:": True}
+    output = client.create_network(input)
+    assert output["id"] == id
 
-    net["admin_state_up"] = False
-    client.update_network(net_id, net)
+    output["admin_state_up"] = False
+    client.update_network(id, output)
+    output = client.get_network(id)
+    assert output["admin_state_up"] is False
 
-    net = client.get_network(net_id)
-    assert net["admin_state_up"] is False
+    client.delete_network(id)
 
-    client.delete_network(net_id)
+    try:
+        client.get_network(id)
+        assert False
+    except exc.HTTPNotFound:
+        pass
+
+
+def test_network_bulk(client):
+    id1 = str(uuid.uuid4())
+    id2 = str(uuid.uuid4())
+    input = [{"id": id1,
+              "name": "name1",
+              "tenant_id": "tenant_id1",
+              "admin_state_up:": True},
+             {"id": id2,
+              "name": "name2",
+              "tenant_id": "tenant_id2",
+              "admin_state_up:": False}]
+
+    output = client.create_network_bulk(input)
+    assert len(output) == 2
+    diff = [x for x in output if x["id"] not in [id1, id2]]
+    assert len(diff) == 0
+
+
+def test_security_group_crud(client):
+    id = str(uuid.uuid4())
+    input = {"id": id,
+             "name": "name1",
+             "tenant_id": "tenant_id",
+             "description:": "description"}
+    output = client.create_security_group(input)
+    assert output["id"] == id
+    assert output["name"] == "name1"
+
+    test_security_group_rule_crud(client, id)
+    test_security_group_rule_bulk(client, id)
+
+    output["name"] = "name2"
+    client.update_security_group(id, output)
+    output = client.get_security_group(id)
+    assert output["name"] == "name2"
+
+    client.delete_security_group(id)
+    try:
+        client.get_security_group(id)
+        assert False
+    except exc.HTTPNotFound:
+        pass
+
+
+def test_security_group_bulk(client):
+    id1 = str(uuid.uuid4())
+    id2 = str(uuid.uuid4())
+    input = [{"id": id1,
+              "name": "name1",
+              "tenant_id": "tenant_id1",
+              "description:": "description1"},
+             {"id": id2,
+              "name": "name2",
+              "tenant_id": "tenant_id2",
+              "description:": "description2"}]
+
+    output = client.create_security_group_bulk(input)
+    assert len(output) == 2
+    diff = [x for x in output if x["id"] not in [id1, id2]]
+    assert len(diff) == 0
+
+    client.delete_security_group(id1)
+    client.delete_security_group(id2)
+
+
+def test_security_group_rule_crud(client, sg_id):
+    id = str(uuid.uuid4())
+    input = {"id": id,
+             "direction": "ingress",
+             "tenant_id": "tenant_id",
+             "ethertype": "ipv4",
+             "port_range_max": 100,
+             "port_range_min": 50,
+             "protocol": "tcp",
+             "remote_ip_prefix": "10.0.0.3/24",
+             "security_group_id": sg_id}
+    output = client.create_security_group_rule(input)
+    assert output["id"] == id
+    assert output["security_group_id"] == sg_id
+
+    client.delete_security_group_rule(id)
+    try:
+        client.get_security_group_rule(id)
+        assert False
+    except exc.HTTPNotFound:
+        pass
+
+
+def test_security_group_rule_bulk(client, sg_id):
+    id1 = str(uuid.uuid4())
+    id2 = str(uuid.uuid4())
+    input = [{"id": id1,
+              "direction": "ingress",
+              "tenant_id": "tenant_id1",
+              "ethertype": "ipv4",
+              "port_range_max": 100,
+              "port_range_min": 50,
+              "protocol": "tcp",
+              "remote_ip_prefix": "10.0.0.3/24",
+              "security_group_id": sg_id},
+             {"id": id2,
+              "direction": "egress",
+              "tenant_id": "tenant_id2",
+              "ethertype": "ipv4",
+              "port_range_max": 200,
+              "port_range_min": 200,
+              "protocol": "icmp",
+              "security_group_id": sg_id}]
+    output = client.create_security_group_rule_bulk(input)
+    assert len(output) == 2
+    diff = [x for x in output if x["id"] not in [id1, id2]]
+    assert len(diff) == 0
+
+    # Check the rules from sg
+    sg2 = client.get_security_group(sg_id)
+    assert sg2["id"] == sg_id
+    assert len(sg2["security_group_rules"]) == 2
+    diff = [x for x in sg2["security_group_rules"]
+            if x["id"] not in [id1, id2]]
+    assert len(diff) == 0
+
+    client.delete_security_group_rule(id1)
+    client.delete_security_group_rule(id2)
 
 
 def main():
@@ -64,7 +213,12 @@ def main():
         project_id = None
 
     client = MidonetClient(uri, username, password, project_id=project_id)
-    test_network_crud(client)
+
+    print '-------- Testing Network -----'
+    test_network(client)
+
+    print '-------- Testing Security Group -----'
+    test_security_group(client)
 
 
 if __name__ == "__main__":
