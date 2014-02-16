159eea59d229247f1ed3cbf5071416b52497b0c0
Ryu Ishimoto <ryu@midokura.com>
2014-02-17 02:12:57 +0900

    Add LB and FW support
    Implement FW and LB interfaces.
    
    Signed-off-by: Ryu Ishimoto <ryu@midokura.com>

--- src/midonetclient/neutron/client.py
+++ src/midonetclient/neutron/client.py
@@ -194,51 +194,137 @@ class SecurityGroup(object):
 
 class Loadbalancer(object):
 
-    def lb_vip_url(self, id):
-        return self._get_neutron()["lb_vip_template"].replace("{id}", id)
+    def vip_url(self, id):
+        return self._get_neutron()["vip_template"].replace("{id}", id)
 
-    def lb_vips_url(self):
-        return self._get_neutron()["lb_vips"]
+    def vips_url(self):
+        return self._get_neutron()["vips"]
 
-    def lb_pool_url(self, id):
-        return self._get_neutron()["lb_pool_template"].replace("{id}", id)
+    def pool_url(self, id):
+        return self._get_neutron()["pool_template"].replace("{id}", id)
 
-    def lb_pools_url(self):
-        return self._get_neutron()["lb_pools"]
+    def pools_url(self):
+        return self._get_neutron()["pools"]
 
-    def lb_member_url(self, id):
-        return self._get_neutron()["lb_member_template"].replace("{id}", id)
+    def member_url(self, id):
+        return self._get_neutron()["member_template"].replace("{id}", id)
 
-    def lb_members_url(self):
-        return self._get_neutron()["lb_members"]
+    def members_url(self):
+        return self._get_neutron()["members"]
 
-    def lb_health_monitor_url(self, id):
-        return self._get_neutron()["lb_health_monitor_template"].replace(
+    def health_monitor_url(self, id):
+        return self._get_neutron()["health_monitor_template"].replace(
             "{id}", id)
 
-    def lb_health_monitors_url(self):
-        return self._get_neutron()["lb_health_monitors"]
+    def health_monitors_url(self):
+        return self._get_neutron()["health_monitors"]
 
-    def lb_health_monitor_pool_url(self, pool_id):
+    def pool_health_monitors_url(self, pool_id):
         return self._get_neutron()[
-            "lb_health_monitor_pool_template"].replace("{pool_id}", pool_id)
+            "pool_health_monitors_template"].replace("{pool_id}", pool_id)
+
+    def pool_health_monitor_url(self, pool_id, health_monitor_id):
+        return self._get_neutron()[
+            "pool_health_monitor_template"].replace(
+            "{pool_id}", pool_id).replace(
+            "{health_monitor_id", health_monitor_id)
 
     def create_vip(self, vip):
         LOG.info("create_vip %r", vip)
-        return self.client.post(self.lb_vips_url(), media_type.LB_VIP,
-                                body=vip)
+        return self.client.post(self.vips_url(), media_type.VIP, body=vip)
 
     def delete_vip(self, id):
         LOG.info("delete_vip %r", id)
-        self.client.delete(self.lb_vip_url(id))
+        self.client.delete(self.vip_url(id))
 
     def get_vip(self, id):
         LOG.info("get_vip %r", id)
-        return self.client.get(self.lb_vip_url(id), media_type.LB_VIP)
+        return self.client.get(self.vip_url(id), media_type.VIP)
 
     def get_vips(self):
         LOG.info("get_vips")
-        return self.client.get(self.lb_vips_url(), media_type.LB_VIPS)
+        return self.client.get(self.vips_url(), media_type.VIPS)
+
+    def update_vip(self, id, vip):
+        LOG.info("update_vip %r", vip)
+        return self.client.put(self.vip_url(id), media_type.VIP, vip)
+
+    def create_pool(self, pool):
+        LOG.info("create_pool %r", pool)
+        return self.client.post(self.pools_url(), media_type.POOL, body=pool)
+
+    def delete_pool(self, id):
+        LOG.info("delete_pool %r", id)
+        self.client.delete(self.pool_url(id))
+
+    def get_pool(self, id):
+        LOG.info("get_pool %r", id)
+        return self.client.get(self.pool_url(id), media_type.POOL)
+
+    def get_pools(self):
+        LOG.info("get_pools")
+        return self.client.get(self.pools_url(), media_type.POOLS)
+
+    def update_pool(self, id, pool):
+        LOG.info("update_pool %r", pool)
+        return self.client.put(self.pool_url(id), media_type.POOL, pool)
+
+    def create_member(self, member):
+        LOG.info("create_member %r", member)
+        return self.client.post(self.members_url(), media_type.MEMBER,
+                                body=member)
+
+    def delete_member(self, id):
+        LOG.info("delete_member %r", id)
+        self.client.delete(self.member_url(id))
+
+    def get_member(self, id):
+        LOG.info("get_member %r", id)
+        return self.client.get(self.member_url(id), media_type.MEMBER)
+
+    def get_members(self):
+        LOG.info("get_members")
+        return self.client.get(self.members_url(), media_type.MEMBERS)
+
+    def update_member(self, id, member):
+        LOG.info("update_member %r", member)
+        return self.client.put(self.member_url(id), media_type.MEMBER, member)
+
+    def create_health_monitor(self, health_monitor):
+        LOG.info("create_health_monitor %r", health_monitor)
+        return self.client.post(self.health_monitors_url(),
+                                media_type.HEALTH_MONITOR,
+                                body=health_monitor)
+
+    def delete_health_monitor(self, id):
+        LOG.info("delete_health_monitor %r", id)
+        self.client.delete(self.health_monitor_url(id))
+
+    def get_health_monitor(self, id):
+        LOG.info("get_health_monitor %r", id)
+        return self.client.get(self.health_monitor_url(id),
+                               media_type.HEALTH_MONITOR)
+
+    def get_health_monitors(self):
+        LOG.info("get_health_monitors")
+        return self.client.get(self.health_monitors_url(),
+                               media_type.HEALTH_MONITORS)
+
+    def update_health_monitor(self, id, health_monitor):
+        LOG.info("update_health_monitor %r", health_monitor)
+        return self.client.put(self.health_monitor_url(id),
+                               media_type.HEALTH_MONITOR, health_monitor)
+
+    def create_pool_health_monitor(self, health_monitor, pool_id):
+        LOG.info("create_pool_health_monitor %r", health_monitor)
+        return self.client.post(self.pool_health_monitors_url(pool_id),
+                                media_type.HEALTH_MONITOR_POOL,
+                                body=health_monitor)
+
+    def delete_pool_health_monitor(self, health_monitor_id, pool_id):
+        LOG.info("delete_pool_health_monitor %r", id)
+        return self.client.delete(self.pool_health_monitor_url(
+            pool_id, health_monitor_id))
 
 
 class Firewall(object):
@@ -269,56 +355,94 @@ class Firewall(object):
         return self._get_neutron()["fw_remove_rule_template"].replace(
             "{firewall_policy_id}", firewall_policy_id)
 
+    def create_firewall(self, firewall):
+        LOG.info("create_firewall %r", firewall)
+        return self.client.post(self.firewalls_url(), media_type.FIREWALL,
+                                body=firewall)
 
-class VpnService(object):
+    def delete_firewall(self, id):
+        LOG.info("delete_firewall %r", id)
+        self.client.delete(self.firewall_url(id))
 
-    def vpn_service_url(self, id):
-        return self._get_neutron()["vpn_service_template"].replace("{id}", id)
+    def get_firewall(self, id):
+        LOG.info("get_firewall %r", id)
+        return self.client.get(self.firewall_url(id), media_type.FIREWALL)
 
-    def vpn_services_url(self):
-        return self._get_neutron()["vpn_services"]
+    def get_firewalls(self):
+        LOG.info("get_firewalls")
+        return self.client.get(self.firewalls_url(), media_type.FIREWALLS)
 
-    def vpn_ike_policy_url(self, id):
-        return self._get_neutron()["vpn_ike_policy_template"].replace(
-            "{id}", id)
+    def update_firewall(self, id, firewall):
+        LOG.info("update_firewall %r", firewall)
+        return self.client.put(self.firewall_url(id), media_type.FIREWALL,
+                               firewall)
 
-    def vpn_ike_policies_url(self):
-        return self._get_neutron()["vpn_ike_policies"]
+    def create_firewall_policy(self, firewall_policy):
+        LOG.info("create_firewall_policy %r", firewall_policy)
+        return self.client.post(self.firewall_policys_url(),
+                                media_type.FIREWALL_POLICY,
+                                body=firewall_policy)
 
-    def vpn_ipsec_policy_url(self, id):
-        return self._get_neutron()["vpn_ipsec_policy_template"].replace(
-            "{id}", id)
+    def delete_firewall_policy(self, id):
+        LOG.info("delete_firewall_policy %r", id)
+        self.client.delete(self.firewall_policy_url(id))
 
-    def vpn_ipsec_policies_url(self):
-        return self._get_neutron()["vpn_ipsec_policies"]
+    def get_firewall_policy(self, id):
+        LOG.info("get_firewall_policy %r", id)
+        return self.client.get(self.firewall_policy_url(id),
+                               media_type.FIREWALL_POLICY)
 
-    def vpn_ipsec_site_connection_url(self, id):
-        return self._get_neutron()[
-            "vpn_ipsec_site_connection_template"].replace("{id}", id)
+    def get_firewall_policies(self):
+        LOG.info("get_firewall_policies")
+        return self.client.get(self.firewall_policies_url(),
+                               media_type.FIREWALL_POLICIES)
 
-    def vpn_ipsec_site_connections_url(self):
-        return self._get_neutron()["vpn_ipsec_site_connections"]
+    def update_firewall_policy(self, id, firewall_policy):
+        LOG.info("update_firewall_policy %r", firewall_policy)
+        return self.client.put(self.firewall_policy_url(id),
+                               media_type.FIREWALL_POLICY,
+                               firewall_policy)
+
+    def create_firewall_rule(self, firewall_rule):
+        LOG.info("create_firewall_rule %r", firewall_rule)
+        return self.client.post(self.firewall_rules_url(),
+                                media_type.FIREWALL_RULE,
+                                body=firewall_rule)
+
+    def delete_firewall_rule(self, id):
+        LOG.info("delete_firewall_rule %r", id)
+        self.client.delete(self.firewall_rule_url(id))
+
+    def get_firewall_rule(self, id):
+        LOG.info("get_firewall_rule %r", id)
+        return self.client.get(self.firewall_rule_url(id),
+                               media_type.FIREWALL_RULE)
+
+    def get_firewall_policies(self):
+        LOG.info("get_firewall_rules")
+        return self.client.get(self.firewall_policies_url(),
+                               media_type.FIREWALL_RULES)
+
+    def update_firewall_rule(self, id, firewall_rule):
+        LOG.info("update_firewall_rule %r", firewall_rule)
+        return self.client.put(self.firewall_rule_url(id),
+                               media_type.FIREWALL_RULE,
+                               firewall_rule)
+
+    def insert_rule(self, firewall_policy_id, rule_info):
+        LOG.info("insert_rule %r", rule_info)
+        return self.client.put(self.fw_insert_rule_url(firewall_policy_id),
+                               media_type.FW_INSERT_RULE,
+                               body=rule_info)
+
+    def remove_rule(self, firewall_policy_id, rule_info):
+        LOG.info("remove_rule %r", rule_info)
+        return self.client.put(self.fw_remove_rule_url(firewall_policy_id),
+                               media_type.FW_REMOVE_RULE,
+                               body=rule_info)
 
 
-class Metering(object):
-
-    def metering_label_url(self, id):
-        return self._get_neutron()["metering_label_template"].replace(
-            "{id}", id)
-
-    def metering_labels_url(self):
-        return self._get_neutron()["metering_labels"]
-
-    def metering_label_rule_url(self, id):
-        return self._get_neutron()["metering_label_rule_template"].replace(
-            "{id}", id)
-
-    def metering_label_rules_url(self):
-        return self._get_neutron()["metering_label_rules"]
-
-
-class MidonetClient(UrlProvider, L3, SecurityGroup, Loadbalancer, Firewall,
-                    VpnService, Metering):
+class MidonetClient(UrlProvider, L3, SecurityGroup, Loadbalancer, Firewall):
 
     def __init__(self, base_uri, username, password, project_id=None):
         self.base_uri = base_uri
--- src/midonetclient/neutron/media_type.py
+++ src/midonetclient/neutron/media_type.py
@@ -46,52 +46,27 @@ SECURITY_GROUP_RULES = \
     "application/vnd.org.midonet.neutron.SecurityGroupRules-v1+json"
 
 # Load balancer
-LB_VIP = "application/vnd.org.midonet.neutron.lb.Vip-v1+json"
-LB_VIPS = "application/vnd.org.midonet.neutron.lb.Vips-v1+json"
-LB_POOL = "application/vnd.org.midonet.neutron.lb.Pool-v1+json"
-LB_POOLS = "application/vnd.org.midonet.neutron.lb.Pools-v1+json"
-LB_MEMBER = "application/vnd.org.midonet.neutron.lb.Member-v1+json"
-LB_MEMBERS = "application/vnd.org.midonet.neutron.lb.Members-v1+json"
-LB_HEALTH_MONITOR = \
-    "application/vnd.org.midonet.neutron.lb.HealthMonitor-v1+json"
-LB_HEALTH_MONITORS = \
-    "application/vnd.org.midonet.neutron.lb.HealthMonitors-v1+json"
-LB_HEALTH_MONITOR_POOL = \
-    "application/vnd.org.midonet.neutron.lb.HealthMonitorPool-v1+json"
+VIP = "application/vnd.org.midonet.neutron.Vip-v1+json"
+VIPS = "application/vnd.org.midonet.neutron.Vips-v1+json"
+POOL = "application/vnd.org.midonet.neutron.Pool-v1+json"
+POOLS = "application/vnd.org.midonet.neutron.Pools-v1+json"
+MEMBER = "application/vnd.org.midonet.neutron.Member-v1+json"
+MEMBERS = "application/vnd.org.midonet.neutron.Members-v1+json"
+HEALTH_MONITOR = \
+    "application/vnd.org.midonet.neutron.HealthMonitor-v1+json"
+HEALTH_MONITORS = \
+    "application/vnd.org.midonet.neutron.HealthMonitors-v1+json"
+HEALTH_MONITOR_POOL = \
+    "application/vnd.org.midonet.neutron.HealthMonitorPool-v1+json"
 
 # Firewall
 FIREWALL = "application/vnd.org.midonet.neutron.Firewall-v1+json"
 FIREWALLS = "application/vnd.org.midonet.neutron.Firewalls-v1+json"
-FW_RULE = "application/vnd.org.midonet.neutron.fw.Rule-v1+json"
-FW_RULES = "application/vnd.org.midonet.neutron.fw.Rules-v1+json"
+FW_RULE = "application/vnd.org.midonet.neutron.FirewallRule-v1+json"
+FW_RULES = "application/vnd.org.midonet.neutron.FirewallRules-v1+json"
 FW_INSERT_RULE = \
-    "application/vnd.org.midonet.neutron.fw.InsertRule-v1+json"
+    "application/vnd.org.midonet.neutron.InsertFirewallRule-v1+json"
 FW_REMOVE_RULE = \
-    "application/vnd.org.midonet.neutron.fw.RemoveRule-v1+json"
-FW_POLICY = "application/vnd.org.midonet.neutron.fw.Policy-v1+json"
-FW_POLICIES = "application/vnd.org.midonet.neutron.fw.Policies-v1+json"
-
-# VPN
-VPN_SERVICE = "application/vnd.org.midonet.neutron.VpnService-v1+json"
-VPN_SERVICES = "application/vnd.org.midonet.neutron.VpnServices-v1+json"
-VPN_IKE_POLICY = "application/vnd.org.midonet.neutron.vpn.IkePolicy-v1+json"
-VPN_IKE_POLICIES = \
-    "application/vnd.org.midonet.neutron.vpn.IkePolicies-v1+json"
-VPN_IPSEC_POLICY = \
-    "application/vnd.org.midonet.neutron.vpn.IpsecPolicy-v1+json"
-VPN_IPSEC_POLICIES = \
-    "application/vnd.org.midonet.neutron.vpn.IpsecPolicies-v1+json"
-VPN_IPSEC_SITE_CONNECTION = \
-    "application/vnd.org.midonet.neutron.vpn.IpsecSiteConnection-v1+json"
-VPN_IPSEC_SITE_CONNECTIONS = \
-    "application/vnd.org.midonet.neutron.vpn.IpsecSiteConnections-v1+json"
-
-# Metering
-METERING_LABEL =\
-    "application/vnd.org.midonet.neutron.metering.Label-v1+json"
-METERING_LABELS = \
-    "application/vnd.org.midonet.neutron.metering.Labels-v1+json"
-METERING_LABEL_RULE = \
-    "application/vnd.org.midonet.neutron.metering.LabelRule-v1+json"
-METERING_LABEL_RULES = \
-    "application/vnd.org.midonet.neutron.metering.LabelRules-v1+json"
+    "application/vnd.org.midonet.neutron.RemoveFirewallRule-v1+json"
+FW_POLICY = "application/vnd.org.midonet.neutron.FirewallPolicy-v1+json"
+FW_POLICIES = "application/vnd.org.midonet.neutron.FirewallPolicies-v1+json"
